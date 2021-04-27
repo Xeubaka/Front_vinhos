711e7eed23bbe32aadd3d67942a3147443b963f7
Luis Roberto <lrs.juca@gmail.com>
2021-04-26 21:31:34 -0300

    Initialize project using Create React App

---
+++ pages/Home/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+function Home() {
+    return ( 
+        <div>
+            <header className='header'>
+                <div className='toolbar'>
+                    <div className=''>
+                        <span className=''>   
+                            Conecta
+                        </span>
+                    </div>
+                    <div className=''>
+                        <button className=''> Novo Vinho </button>
+                        <span className=''> img1 </span> 
+                        <span className=''> img2 </span>
+                    </div>
+                </div>
+            </header>
+            <main className=''>
+                main
+            </main>
+        </div>    
+    );
+}
+
+export default Home;
